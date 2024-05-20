31526a371f69fe38c87576e8ed407e06d5835ac9
Danilemon73 <danielalarconsoto@gmail.com>
2024-05-20 16:13:50 -0500

    Version 3.2.1

--- src/DataAccessObject/CasoMedicoDAO.java
+++ src/DataAccessObject/CasoMedicoDAO.java
@@ -1,4 +1,5 @@
 package DataAccessObject;
 
 public class CasoMedicoDAO {
+
 }
--- src/Interfaces/InterfazCasoEmergencia.java
+++ src/Interfaces/InterfazCasoEmergencia.java
@@ -1,4 +1,13 @@
 package Interfaces;
 
-public class InterfazCasoEmergencia {
+import Negocio.CasoEmergencia;
+import java.util.List;
+
+public interface InterfazCasoEmergencia {
+    void agregarCasoEmergencia(CasoEmergencia casoEmergencia);
+    void eliminarCasoEmergencia(long id);
+    List<CasoEmergencia> listarCasosEmergencia();
+    void guardarDatos();
+    void cargarDatos();
 }
+
