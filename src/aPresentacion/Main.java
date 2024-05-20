21156df907d12b9dae4ac0982ffae84d2f18ee9c
Danilemon73 <danielalarconsoto@gmail.com>
2024-05-19 19:22:39 -0500

    Version 3.1

--- src/aPresentacion/Main.java
+++ src/aPresentacion/Main.java
@@ -6,5 +6,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Al fin descubri como usar esta mierda de GitHub");
+
+        System.out.println("Al fin me funcionó esta mierda");
     }
 }
